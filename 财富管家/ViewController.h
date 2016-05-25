0031e8d85ff7ea8f6facf8c0248b54866274f8df
anbook <anbook@anbo.local>
2016-05-25 11:01:09 +0800

    Initial Commit

---
+++ 财富管家/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  财富管家
+//
+//  Created by anbook on 16/5/25.
+//  Copyright © 2016年 anbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 财富管家/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  财富管家
+//
+//  Created by anbook on 16/5/25.
+//  Copyright © 2016年 anbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
